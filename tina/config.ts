ca2ca5e70336e546399d2bc375b123fb8be6fee6
picafe <tallapaneni.surya@gmail.com>
2023-10-20 19:30:49 -0400

    feat: tinacms cloud, restructured website fix: package scripts, rss, links

--- tina/config.ts
+++ tina/config.ts
@@ -6,8 +6,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
 export default defineConfig({
 	branch,
-	clientId: null, // Get this from tina.io
-	token: null, // Get this from tina.io
+	clientId: import.meta.env.PUBLIC_TINACMS_CLIENTID,
+	token: import.meta.env.PUBLIC_TINACMS_CONTENT_TOKEN,
 
 	build: {
 		outputFolder: 'admin',
