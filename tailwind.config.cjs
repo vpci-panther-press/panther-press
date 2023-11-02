41ca4936203537ad72ca01ee4b52da68ad26b25a
Surya <tallapaneni.surya@gmail.com>
2023-11-02 02:34:18 +0000

    feat: new navbar, favicon (not final)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,8 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				white: '#f8f9fa'
+				white: '#f8f9fa',
+				primary: '#470d0d'
 			},
 			fontFamily: {
 				body: ['Manrope', ...defaultTheme.fontFamily.sans]
