b68905b97aa1aa010311c9be4aab433f60395135
Surya <tallapaneni.surya@gmail.com>
2023-11-22 15:12:31 +0000

    fix: viewtransitions (from upstream) fix: links add: images for photography feat: issues page, photography page (unfinished) feat: homepage update fix: preline npm package

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,13 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,md,mdx,ts}'],
 	theme: {
 		extend: {
+			backgroundSize: {
+				'size-200': '200% 150%',
+			},
+			backgroundPosition: {
+				'pos-0': '0% 0%',
+				'pos-100': '100% 100%',
+			},
 			colors: {
 				white: '#f8f9fa',
 				primary: '#d62828',
