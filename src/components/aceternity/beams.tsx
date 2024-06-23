7f6cfc2e36e46d8e1051991c5b9e93a94f66ffa8
Surya <tallapaneni.surya@gmail.com>
2024-06-23 18:11:02 +0000

    fix: beams styling

--- src/components/aceternity/beams.tsx
+++ src/components/aceternity/beams.tsx
@@ -35,7 +35,7 @@ export const TracingBeam = ({
 
 	return (
 		<motion.div ref={ref} className={cn('relative w-full h-full', className)}>
-			<div className='absolute -left-4 md:-left-20 top-3'>
+			<div className='absolute md:-left-20' style={{ zIndex: 999, left: -8, top: 26 }}>
 				<motion.div
 					transition={{
 						duration: 0.2,
@@ -44,7 +44,8 @@ export const TracingBeam = ({
 					animate={{
 						boxShadow: scrollYProgress.get() > 0 ? 'none' : 'rgba(0, 0, 0, 0.24) 0px 3px 8px'
 					}}
-					className='ml-[27px] h-4 w-4 rounded-full border border-netural-200 shadow-sm flex items-center justify-center'
+					className='h-4 w-4 rounded-full border border-netural-200 shadow-sm flex items-center justify-center'
+					style={{ marginLeft: 10.9 }}
 				>
 					<motion.div
 						transition={{
